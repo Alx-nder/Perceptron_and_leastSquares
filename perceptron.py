c4c2e00199c70bf0a9052e675f40663e91cdfb6e
Alx-nder <tyreekalx@gmail.com>
2024-01-24 17:02:17 -0600

    perceptron done

--- perceptron.py
+++ perceptron.py
@@ -1,41 +1,36 @@
 
 counter =0
-w=[
-    [0,0,0]
-]
+w=[0,0,0]
 
-def dotProduct(v1,v2):
+def dotProduct(wv,fv,c):
+    if c==2:
+        fv=[-v for v in fv]
     res=0
-    for i,j in zip(v1, v2):
+    for i,j in zip(wv, fv):
         res+=i*j
-    # res=[i*j  for i,j in zip(v1, v2)]
     return res
 
 def newWeight(w,fv,c):
     if c==2:
         fv=[-v for v in fv]
     newWV=[w+v for w,v in zip(w,fv)]
-    w.append(newWV)
+    return newWV
 
 classes = [[[0,0],[0,1]],
            [[1,0],[1,1]]]
 
-def epoch(classes,w):
+while counter < len(classes[0])+len(classes[1]) +1:
+    print("epoch",counter)
     for c in range(len(classes)):
         for fv in classes[c]:
-            if len(fv)<len(w[-1]):
+            if len(fv)<len(w):
                 fv.append(1)
-            if dotProduct(w[-1],fv)>0:
-                pass
-            else:
-                newWeight(w[-1],fv,c+1)
-    return 1
 
-def perceptron(counter):
-    while counter < len(classes[0])+len(classes[1]) +1:
-        epoch(classes,w)
-        counter+=epoch
-    
-            
+            if dotProduct(w,fv,c+1)<=0:
+                temp=newWeight(w,fv,c+1)
+                w=temp
+                counter=0 
+    counter+=1
+print(w)
 
 
