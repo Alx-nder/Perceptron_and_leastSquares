9ba3ebb876bddbd7a12dca33d8d76e9c5d5d4e97
Alx-nder <tyreekalx@gmail.com>
2024-02-05 15:09:40 -0600

    find x,y,z

--- generic_perceptron.py
+++ generic_perceptron.py
@@ -1,4 +1,5 @@
 
+# w=[1.00000000e+00,2.22044605e-16,-1.11022302e-16]
 w=[0,0,0]
 classes = [[[0,0],[0,1]], [[1,0],[1,1]]]
 ro=1
@@ -34,8 +35,8 @@ while counter < len(classes[0])+len(classes[1]) +1:
                 wv.append(w)
                 counter=0 
     counter+=1
-print(wv)
-
+print(wv[-1][0])
+w=wv[-1]
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -47,7 +48,7 @@ ax=plt.axes(projection='3d')
 ax=fig.add_subplot(111,projection='3d')
 
 xx,yy=np.meshgrid(range(-2,3),range(-2,3))
-z=(2*-xx)
+z=w[0]*-xx  -w[1]*yy
 
 ax.plot_surface(xx, yy, z, alpha=0.5)
 
--- least_squares.py
+++ least_squares.py
@@ -2,14 +2,24 @@ import numpy as np
 
 classes = [[[0,0,1],[0,1,1]], [[1,0,1],[1,1,1]]]
 
+t=[[0],[0],[1],[1]]
+
 fv=classes[0]
 fv.append(classes[1][0])
 fv.append(classes[1][1])
 
-# transpose matrix X
+#Xt: transpose matrix X
 Xt=np.array(fv).transpose()
 
-# 
-XtXdot_prot=sum([np.dot(x,x) for x in fv])
+#XtX: matrix multiplication
+XtX=np.matmul(Xt,fv)
 
-# w=[1/XtXdot_prot * ]
+#XtX_inv: inverse of XtX
+XtX_inv=np.linalg.inv(XtX)
+
+#Xfin (XtX)**-1 multiplied by Xt
+Xfin= np.matmul(XtX_inv,Xt)
+
+
+w=Xfin.dot(t)
+print(w)
