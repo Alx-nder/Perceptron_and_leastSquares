5f2e28e5f47edc062fb07fd17cdb616280bea4d6
Alx-nder <tyreekalx@gmail.com>
2024-02-05 15:46:02 -0600

    works

--- least_squares.py
+++ least_squares.py
@@ -2,15 +2,16 @@ import numpy as np
 
 classes = [[[0,0,1],[0,1,1]], [[1,0,1],[1,1,1]]]
 
-t=[[0],[0],[1],[1]]
+t=[1,1,0,0]
 
 fv=classes[0]
 fv.append(classes[1][0])
 fv.append(classes[1][1])
 
+fv=np.array(fv)
+
 #Xt: transpose matrix X
 Xt=np.array(fv).transpose()
-
 #XtX: matrix multiplication
 XtX=np.matmul(Xt,fv)
 
@@ -21,5 +22,8 @@ XtX_inv=np.linalg.inv(XtX)
 Xfin= np.matmul(XtX_inv,Xt)
 
 
-w=Xfin.dot(t)
-print(w)
+# w=Xfin.dot(t)
+w=np.matmul(Xfin,t)
+
+
+print(w.dot([1,1,1]))
