72dc5efb9c360f2b244a41d4f51a30342b394e64
Alx-nder <tyreekalx@gmail.com>
2024-02-05 15:15:23 -0600

    reset series

--- generic_perceptron.py
+++ generic_perceptron.py
@@ -1,5 +1,4 @@
 
-# w=[1.00000000e+00,2.22044605e-16,-1.11022302e-16]
 w=[0,0,0]
 classes = [[[0,0],[0,1]], [[1,0],[1,1]]]
 ro=1
@@ -35,8 +34,8 @@ while counter < len(classes[0])+len(classes[1]) +1:
                 wv.append(w)
                 counter=0 
     counter+=1
-print(wv[-1][0])
-w=wv[-1]
+print(wv)
+
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -48,7 +47,7 @@ ax=plt.axes(projection='3d')
 ax=fig.add_subplot(111,projection='3d')
 
 xx,yy=np.meshgrid(range(-2,3),range(-2,3))
-z=w[0]*-xx  -w[1]*yy
+z=(2*-xx)
 
 ax.plot_surface(xx, yy, z, alpha=0.5)
 
