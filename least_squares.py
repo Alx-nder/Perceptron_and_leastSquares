ba904f1546c1030687f920fbd24e25ff157497dd
Alx-nder <tyreekalx@gmail.com>
2024-02-05 23:36:23 -0600

    interesting stats

--- least_squares.py
+++ least_squares.py
@@ -2,7 +2,7 @@ import numpy as np
 
 classes = [[[0,0,1],[0,1,1]], [[1,0,1],[1,1,1]]]
 
-t=[1,1,0,0]
+t=[0,0,1,1]
 
 fv=classes[0]
 fv.append(classes[1][0])
@@ -24,6 +24,27 @@ Xfin= np.matmul(XtX_inv,Xt)
 
 # w=Xfin.dot(t)
 w=np.matmul(Xfin,t)
-
-
+print(w)
+# test
 print(w.dot([1,1,1]))
+
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig=plt.figure(figsize=(5,5))
+ax=plt.axes(projection='3d')
+
+ax=fig.add_subplot(111,projection='3d')
+
+xx,yy=np.meshgrid(range(-2,3),range(-2,3))
+z=(w[0]*xx + w[1]*yy)
+
+ax.plot_surface(xx, yy, z, alpha=0.5)
+
+ax.scatter3D([0,0],[0,1],[1,1],color='green')
+ax.scatter3D([-1,-1],[0,-1],[-1,-1], color='red')
+
+# function to show the plot
+plt.show()
