c66fc5b44d4891618e2bc03d6f4154e674fb202b
Alx-nder <tyreekalx@gmail.com>
2024-02-07 10:44:19 -0600

    plotted

--- least_squares.py
+++ least_squares.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def leastSquares(target,feature):
@@ -40,6 +42,51 @@ def leastSquares(target,feature):
     Xfin= np.matmul(XtX_inv,Xt)
 
     w=np.matmul(Xfin,t)
-    return w
+    # return w
 
-print(leastSquares([1,2,3],[1,2,3,4]))
+    # test
+    misclassed=0
+    for x in range(len(fv)):
+        # first class <= 0.5, second class >= 0.5
+        if (w.dot(fv[x])<=0.5 and t[x]==0) or (w.dot(fv[x])>0.5 and t[x]==1):
+            pass
+        else:
+            misclassed+=1
+
+    # plot 
+    if len(needed_features)==3:
+        fig=plt.figure(figsize=(4,4))
+        ax=plt.axes(projection='3d')
+
+        ax=fig.add_subplot(projection='3d')
+
+        xx,yy=np.meshgrid(range(0,9),range(0,9))
+        z=(w[0]*xx + w[1]*yy)/w[2]
+
+        ax.plot_surface(xx, yy, z, alpha=0.5)
+        
+        # x'es
+        c1_start=(target[0]-1)*class_size
+        c1_stop=c1_start+class_size
+        c1_plot=[x for x in fv[c1_start:c1_stop]]
+        for point in c1_plot:
+            ax.scatter3D(point[0],point[1],point[2],color='green')
+        
+        c2_start=(target[1]-1)*class_size
+        c2_stop=c2_start+class_size
+        c2_plot=[x for x in fv[c2_start:c2_stop]]
+        for point in c2_plot:
+            ax.scatter3D(point[0],point[1],point[2],color='red')
+
+        c3_start=(target[2]-1)*class_size
+        c3_stop=c3_start+class_size
+        c3_plot=[x for x in fv[c3_start:c3_stop]]
+        for point in c3_plot:
+            ax.scatter3D(point[0],point[1],point[2],color='orange')
+
+    # function to show the plot
+        plt.show()
+    
+    return w,misclassed
+
+print(leastSquares([1,2,3],[1,2]))
