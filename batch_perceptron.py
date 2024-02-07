81f9eb6b90dad64736cea583940f4b7073be8652
Alx-nder <tyreekalx@gmail.com>
2024-02-06 23:37:36 -0600

    next, multiclass

--- batch_perceptron.py
+++ batch_perceptron.py
@@ -4,26 +4,23 @@ import pandas as pd
 
 
 def batch_perceptron(target,feature, ro):
-    # ['setosa', 'versicolor', 'virginica']
-    feature=[f-1 for f in feature]
-    
+    feature=[f-1 for f in feature] 
     feature.append(-1)
     
-    df=pd.read_excel("Pattern_Recognition\proj1\Proj1DataSet.xlsx")
-    # add offset  
+    df=pd.read_excel("Pattern_Recognition\\proj1\\Proj1DataSet.xlsx")
+    # add offset column 
     df=df.assign(offset=[1]*df.shape[0])
 
-    classes=[(df.loc[df['species'] ==fv]).drop(columns='species').values.tolist() for fv in df.species.unique()]
+    # separate features we need
+    needed_features = [df.columns[x] for x in feature ]
 
-    # drop species to get feature variables fv or X
-    df.drop(columns='species',inplace=True)
+    classes=[(df.loc[df['species'] ==fv])[needed_features].values.tolist() for fv in df.species.unique()]
 
     classes=[classes[target[0]],classes[target[1]]+classes[target[2]]]
 
     # class 2 negative
     classes[1]=-1*np.array(classes[1])
 
-    
     # initial guess for weight vector: all zeros
     w=np.array([0]*len(feature))
 
@@ -35,8 +32,6 @@ def batch_perceptron(target,feature, ro):
         
         for c in range(len(classes)):
             for fv in classes[c]:
-                # separate features we need
-                fv=[fv[new_fv] for new_fv in feature]
                 
                 if w.dot(fv)<=0:
                     misclas_fv=np.add(fv,misclas_fv)
@@ -52,9 +47,5 @@ def batch_perceptron(target,feature, ro):
             w=np.add(w, (ro*np.array(misclas_fv)))
             count+=1
     return(count,w)
-
-
-
-
 
 print(batch_perceptron([0,1,2],[3,4],.5))
--- least_squares.py
+++ least_squares.py
@@ -3,28 +3,26 @@ import pandas as pd
 
 
 def leastSquares(target,feature):
+
     feature=[f-1 for f in feature]
     feature.append(-1)
 
-
     df=pd.read_excel("Pattern_Recognition\proj1\Proj1DataSet.xlsx")
+    
     # add offset and drop species to get feature variables fv or X
     df=df.assign(offset=[1]*df.shape[0])
 
     needed_features = [df.columns[x] for x in feature ]
 
+    class_size=df.loc[df.species==df.species.unique()[0]].shape[0]
 
-    classes=[(df.loc[df['species'] ==fv][needed_features]).values.tolist() for fv in df.species.unique()]
-
-    # drop species to get feature variables fv or X
-
-    
-
-    # class separation
-    classes=[classes[target[0]],classes[target[1]]+classes[target[2]]]
-
-    # target vector 
-    t=[0]*len(classes[0])+[1]*len(classes[1])
+    # target vector ... order matters
+    if target[0]==1:
+        t=[0]*class_size + [1]*(df.shape[0]-class_size)
+    elif target[0]==2:
+        t=[1]*class_size + [0]*class_size +[1]*class_size
+    else:
+        t= [1]*(df.shape[0]-class_size)+ [0]*class_size
 
     # feature vectors fv or X extracted from dataframe
     fv=df[needed_features].values.tolist()
@@ -44,4 +42,4 @@ def leastSquares(target,feature):
     w=np.matmul(Xfin,t)
     return w
 
-print(leastSquares([0,1,2],[3,4]))
+print(leastSquares([1,2,3],[1,2,3,4]))
